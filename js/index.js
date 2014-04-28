336b6513b4114a17e210368bf593208558fa64f6
Alice <futurekids53@gmail.com>
2014-04-28 22:53:02 +0800

    upload first time

---
+++ js/index.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+      $('.covers').hide();
+      $('.covers').fadeIn(2000);
+      
+      $('.sub_covers').hide();
+      $('.sub_covers').fadeIn(1000);
+      
+
+});
