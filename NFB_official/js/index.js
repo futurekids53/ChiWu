5cc33ba3b265e2116c04902ec3ffb5913e0fd76a
Alice <futurekids53@gmail.com>
2014-08-26 16:14:01 +0800

    volume test

--- NFB_official/js/index.js
+++ NFB_official/js/index.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
 
+		$("#ytplayer").setVolume(0);
  
 });
--- NFB_official/js/maincha.js
+++ NFB_official/js/maincha.js
@@ -2,6 +2,5 @@ $(document).ready(function() {
  
   $("#owl-example").owlCarousel();
 
-	$("#ytplayer").setVolume(0);
  
 });
